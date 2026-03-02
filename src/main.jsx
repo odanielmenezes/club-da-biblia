836e3023d730489a4e67e39a865a7554993c8d10
Daniel Menezes <danielprofissional93@outlook.com>
2026-03-02 10:02:35 -0300

    setup inicial gith

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import App from './App.jsx'
 import GlobalStyle from './styles/GlobalStyle'
 import theme from './styles/Theme'
 
+window.SC_DISABLE_SPEEDY = true
+window.__CLUB_BUILD__ = '2026-03-02.3'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
